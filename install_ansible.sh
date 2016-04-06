dfb8a2d1c4c9091f4b4d8dd2363a92e68bef6b91
root <root@localhost.localdomain>
2016-04-06 18:58:50 -0400

    added -y to install; commented out wwnodescan for now

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-yum install python-devel python-setuptools python-setuptools-devel gcc 
+yum -y install python-devel python-setuptools python-setuptools-devel gcc 
 easy_install pip
 #pexpect has to be 3.3 because new 4.01 version only
 # works with python >= 2.7 :(
 pip install paramiko PyYAML Jinja2 httplib2 six pexpect==3.3
 #moved this after lib installations
-git clone git://github.com/ansible/ansible.git --recursive
-source ./ansible/hacking/env-setup -q
-echo -e "\nsource ./ansible/hacking/env-setup -q" >> .bashrc
+git clone git://github.com/ansible/ansible.git --recursive $HOME/ansible/
+source $HOME/ansible/hacking/env-setup -q
+echo -e "\nsource $HOME/ansible/hacking/env-setup -q" >> $HOME/.bashrc
 echo "# Ansible Inventory" > inventory
 echo "[headnode]" >> inventory
 echo "localhost ansible_connection=local" >> inventory
 mkdir -p /etc/ansible
 mv inventory /etc/ansible/hosts
-./ansible/bin/ansible headnode -a "/bin/hostname"
+$HOME/ansible/bin/ansible headnode -a "/bin/hostname"
